aa7022bd44bf7101bfb3521ed1fa3e641bc1f203
Mauricio Cousillas <cousillas806@gmail.com>
2018-04-25 15:08:00 -0300

    Added basic folder structure and navigation setup (#2)

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-import { AppRegistry } from 'react-native';
-import App from './App';
+import App from './src/App';
 
-AppRegistry.registerComponent('ReactNativeTemplate', () => App);
+App.startApp();
