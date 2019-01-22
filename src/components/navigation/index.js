c7aa261e4da3571519c3872b4cdea1a8c8db2ace
Mario Souto <mariosouto@outlook.com>
2019-01-22 10:26:36 -0300

    Upgrade to React Navigation 3 (#36)
    Also:
     - Bump RN version to 0.57.8
     - Bump React and React-Test-Renderer to 16.6.3

--- src/components/navigation/index.js
+++ src/components/navigation/index.js
@@ -1,15 +1,17 @@
-import { createSwitchNavigator } from 'react-navigation';
+import { createAppContainer, createSwitchNavigator } from 'react-navigation';
 import AuthHandler from './AuthHandler';
 import Auth from './AuthNavigator';
 import App from './AppNavigator';
 
-export default createSwitchNavigator(
-  {
-    App,
-    Auth,
-    AuthHandler,
-  },
-  {
-    initialRouteName: 'AuthHandler',
-  },
-);
+export default createAppContainer( // eslint-disable-line
+  createSwitchNavigator(
+    {
+      App,
+      Auth,
+      AuthHandler,
+    },
+    {
+      initialRouteName: 'AuthHandler',
+    },
+  ),
+); // eslint-disable-line
