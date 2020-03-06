05023ee5622c7f9545d2ee51534e4cb465bf5104
Joaquin <jngalletto@gmail.com>
2020-03-06 15:01:41 -0300

    Fix login request (#45)
    * add resolve or reject to login http call
    * fix space
    * remove commented code

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,27 +1,9 @@
-// import httpClient from './HttpClient';
-
 class UserController {
   constructor() {
     this.basePath = '/users';
   }
 
   login = async (email, password) =>
-  // Real implementation of a login request using the HttpClient
-  /* try {
-    const result = await httpClient.post({
-      url: `${this.basePath}/session`,
-      method: 'POST',
-      data: {
-        email,
-        password,
-      },
-    });
-    return result.data.user;
-    // Data is the object exposes by axios for the response json
-  } catch (error) {
-    return error;
-  }
-  */
     // This is a mocked example to simulate api behavior
     new Promise((resolve, reject) => {
       if (email !== 'a@a.com' && password !== '') {
