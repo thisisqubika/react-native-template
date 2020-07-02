809dba1e1dc6678db891898f5b50c6ab3093f5af
Donato Aguirre <donato@Donatos-MacBook-Pro.local>
2020-07-02 10:53:09 -0300

    Remove animation

--- src/App.js
+++ src/App.js
@@ -5,19 +5,17 @@ import { NavigationContainer } from '@react-navigation/native';
 import { AppearanceProvider, useColorScheme } from 'react-native-appearance';
 import RNBootSplash from 'react-native-bootsplash';
 import { enableScreens } from 'react-native-screens';
-import { store, persist } from './reducers';
+import { store, persist } from 'reducers';
 import Navigation from 'components/navigation';
 import { ColorScheme, DarkTheme, LightTheme } from 'helpers/Themes';
 
 enableScreens();
 
-export default function App() {
+function App() {
   const scheme = useColorScheme();
 
   useEffect(() => {
-    persist(() => {
-      RNBootSplash.hide({ duration: 500 });
-    });
+    persist(RNBootSplash.hide);
   }, []);
 
   return (
@@ -32,3 +30,5 @@ export default function App() {
     </Provider>
   );
 }
+
+export default App;
