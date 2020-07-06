29dd155512c6687a4f2b739f48075e80e6042722
Donato Aguirre <donatoaguirre24@gmail.com>
2020-07-06 11:51:15 -0300

    Improve Android build (#62)
    * Add pull request template
    
    * Enable Hermes and Proguard
    
    * Update CircleCi Node.js image (drop node 8 support)
    
    * Update dependencies
    
    Co-authored-by: Donato Aguirre <donato@Donatos-MacBook-Pro.local>

---
+++ .github/pull_request_template.md
@@ -0,0 +1,20 @@
+#### :page_facing_up: Description:
+
+A good description of the problem this PR addresses.
+
+---
+
+#### :pushpin: Notes:
+
+* Warnings, things other developers need to be aware of when merging, etc.
+
+---
+
+#### :heavy_check_mark: Performed Tasks:
+
+* The list of tasks you performed to help the reviewer.
+
+---
+
+#### :play_or_pause_button: Demo:
+* Video or image.
--- src/reducers/UserReducer.js
+++ src/reducers/UserReducer.js
@@ -6,15 +6,8 @@ const initialState = {
 
 const userReducer = (state = initialState, action) => {
   switch (action.type) {
-    case actionTypes.LOGIN_REQUEST:
-      return {
-        ...state,
-      };
     case actionTypes.LOGIN_SUCCESS:
-      return {
-        ...state,
-        user: action.user,
-      };
+      return { ...state, user: action.user };
     case actionTypes.LOGOUT:
       return initialState;
     default:
