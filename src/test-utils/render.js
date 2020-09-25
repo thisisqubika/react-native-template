e960ed8145cdfc8e92cc46f3932a56bb889dbc49
Donato Aguirre <donatoaguirre24@gmail.com>
2020-09-25 14:46:11 -0300

    Fix review comment

--- src/test-utils/render.js
+++ src/test-utils/render.js
@@ -1,8 +1,7 @@
-/* eslint-disable import/no-extraneous-dependencies */
 import { render } from '@testing-library/react-native';
 import React from 'react';
 import { Provider } from 'react-redux';
-import configureStore from './store';
+import configureStore from 'test-utils/store';
 
 export function renderWithProviders(component, { initialState } = {}) {
   const store = configureStore(initialState);
