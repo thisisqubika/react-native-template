f42497fb9fa62ef398c7d87d61d97c18872274e0
Donato Aguirre <donatoaguirre24@gmail.com>
2021-01-27 18:43:15 -0300

    Update dependencies

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,11 @@
 module.exports = {
   env: {
-    es2020: true,
     jest: true,
   },
   extends: [
     '@react-native-community',
     'plugin:import/recommended',
-    'plugin:react-native-a11y/basic',
+    'plugin:react-native-a11y/all',
     'plugin:testing-library/recommended',
     'plugin:prettier/recommended',
     'prettier/react',
@@ -15,12 +14,10 @@ module.exports = {
   parser: 'babel-eslint',
   root: true,
   rules: {
+    'import/newline-after-import': 'error',
+    'import/order': 'error',
     'module-resolver/use-alias': 'error',
     'no-console': 'warn',
-    'import/order': [
-      'error',
-      { groups: ['builtin', 'external', 'parent', 'sibling', 'index'] },
-    ],
   },
   settings: {
     'import/ignore': ['node_modules/react-native/index\\.js$'],
