9d79c87349ce21aa99e93d388d47da927f1c29bc
Federico Martinez <fmartinez0198@gmail.com>
2022-09-21 10:48:11 -0300

    update react-native-reanimated library

--- src/App.js
+++ src/App.js
@@ -3,9 +3,16 @@ import { hide } from 'react-native-bootsplash';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
+import { StyleSheet } from 'react-native';
+import { RootNavigator } from './navigation';
 import { persistor, store } from '@/store';
 import { networkService } from '@/networking';
-import { RootNavigator } from '@/navigation';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
 
 export function App() {
   const handleStoreRehydration = () => {
@@ -21,7 +28,7 @@ export function App() {
   return (
     <Provider store={store}>
       <PersistGate onBeforeLift={handleStoreRehydration} persistor={persistor}>
-        <GestureHandlerRootView style={{ flex: 1 }}>
+        <GestureHandlerRootView style={styles.container}>
           <RootNavigator />
         </GestureHandlerRootView>
       </PersistGate>
