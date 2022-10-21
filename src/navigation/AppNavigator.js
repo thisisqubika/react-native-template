02a9f5a2aa04433b4db6df5cfc8d0782c756173c
Fede Martinez <89869262+fmartinez98@users.noreply.github.com>
2022-10-21 10:23:34 -0300

    Maintenance and dependencies update

--- src/navigation/AppNavigator.js
+++ src/navigation/AppNavigator.js
@@ -2,9 +2,9 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { useTheme } from '@react-navigation/native';
 import React from 'react';
 import { TabBarIcon } from '@/components';
-import { NAVIGATION } from '@/constants';
 import { HomeNavigator } from '@/navigation/HomeNavigator';
 import { ProfileNavigator } from '@/navigation/ProfileNavigator';
+import { TABS } from '@/constants/navigation';
 
 const Tab = createBottomTabNavigator();
 
@@ -20,8 +20,8 @@ export function AppNavigator() {
         tabBarIcon: ({ color }) => <TabBarIcon color={color} routeName={route.name} />,
       })}
     >
-      <Tab.Screen name={NAVIGATION.home} component={HomeNavigator} />
-      <Tab.Screen name={NAVIGATION.profile} component={ProfileNavigator} />
+      <Tab.Screen name={TABS.home} component={HomeNavigator} />
+      <Tab.Screen name={TABS.profile} component={ProfileNavigator} />
     </Tab.Navigator>
   );
 }
